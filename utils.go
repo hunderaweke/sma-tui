56e6a57a937f47417baee24904e3849e125e08af
Hundera Awoke <hunderaweke@gmail.com>
2025-12-19 16:18:39 +0300

    initial: starting the project

---
+++ utils.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/ProtonMail/gopenpgp/v3/crypto"
+	"github.com/ProtonMail/gopenpgp/v3/profile"
+)
+
+type PGPHandler struct {
+	pgp *crypto.PGPHandle
+}
+
+func NewPGPHandler() *PGPHandler {
+	pgp := crypto.PGPWithProfile(profile.RFC9580())
+	return &PGPHandler{
+		pgp: pgp,
+	}
+}
+
+func (h *PGPHandler) ParsePrivateKey(privateKey string) (*crypto.Key, error) {
+	key, err := crypto.NewPrivateKeyFromArmored(privateKey, []byte{})
+	if err != nil {
+		return nil, fmt.Errorf("error in parsing private key: %w", err)
+	}
+	return key, nil
+}
+func (h *PGPHandler) Encrypt(msg string, publicKey *crypto.Key) (string, error) {
+	handle, err := h.pgp.Encryption().Recipient(publicKey).New()
+	if err != nil {
+		return "", err
+	}
+	pgpMessage, err := handle.Encrypt([]byte(msg))
+	if err != nil {
+		return "", err
+	}
+	bytes, err := pgpMessage.ArmorBytes()
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(bytes), nil
+}
+
+func (h *PGPHandler) Decrypt(msg string, key *crypto.Key) (string, error) {
+	raw, err := base64.StdEncoding.DecodeString(msg)
+	if err != nil {
+		return "", err
+	}
+	decHandle, err := h.pgp.Decryption().DecryptionKey(key).New()
+	if err != nil {
+		return "", err
+	}
+	decMsg, err := decHandle.Decrypt(raw, crypto.Armor)
+	if err != nil {
+		return "", err
+	}
+	return string(decMsg.Bytes()), nil
+}
+func (h *PGPHandler) GenerateKey() (*crypto.Key, error) {
+	key, err := h.pgp.KeyGeneration().New().GenerateKey()
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func (h *PGPHandler) ParsePublicKey(armoredPubKey string) (*crypto.Key, error) {
+	pubKey, err := crypto.NewKeyFromArmored(armoredPubKey)
+	if err != nil {
+		return nil, err
+	}
+	return pubKey, nil
+}
